2c1928275e65d12f130dd540cec7e4dffc4a277e
servicemegascale <service.megascale@gmail.com>
2025-11-07 11:10:27 +0530

    Update main

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@
 
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
